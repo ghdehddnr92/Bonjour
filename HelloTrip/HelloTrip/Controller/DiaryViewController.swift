d38ad0bad034276f3c236c5f6b59b21b8d1f07aa
ghdhddnr92 <ghdehddnr92@gmail.com>
2017-11-26 20:00:46 +0900

    create Diary View

--- HelloTrip/HelloTrip/Controller/DiaryViewController.swift
+++ HelloTrip/HelloTrip/Controller/DiaryViewController.swift
@@ -1,42 +1,38 @@
 //
-//  ViewController.swift
+//  DiaryViewController.swift
 //  HelloTrip
 //
-//  Created by 가민경 on 2017. 11. 17..
+//  Created by dong wook on 2017. 11. 26..
 //  Copyright © 2017년 SSU. All rights reserved.
 //
 
 import UIKit
 
-
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class DiaryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     @IBOutlet var floatbutton: UIButton!
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         tableView.delegate = self
         tableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 6
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-          let cellIdentifier = "HomeCell"
-          let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! HomeTableViewCell
+        let cellIdentifier = "DiaryCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! DiaryTableViewCell
         return cell
     }
     
     
-
 }
-
