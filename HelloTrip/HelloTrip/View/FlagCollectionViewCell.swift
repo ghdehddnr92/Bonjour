9c1aa4047452945b30d2df9c78120f7f56478f2b
ghdhddnr92 <ghdehddnr92@gmail.com>
2017-12-07 18:11:51 +0900

    change collectionView

---
+++ HelloTrip/HelloTrip/View/FlagCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FlagCollectionViewCell.swift
+//  HelloTrip
+//
+//  Created by dong wook on 2017. 12. 7..
+//  Copyright © 2017년 SSU. All rights reserved.
+//
+
+import UIKit
+
+class FlagCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var FlagImageView: UIImageView!
+    
+}
