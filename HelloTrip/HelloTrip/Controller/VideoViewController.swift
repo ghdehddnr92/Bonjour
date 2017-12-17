2e180bcd0478faaa5d32ab19292489ea25e17b85
ghdhddnr92 <ghdehddnr92@gmail.com>
2017-12-18 00:39:03 +0900

    Add gallery video camera

---
+++ HelloTrip/HelloTrip/Controller/VideoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VideoViewController.swift
+//  HelloTrip
+//
+//  Created by dong wook on 2017. 12. 18..
+//  Copyright © 2017년 SSU. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
